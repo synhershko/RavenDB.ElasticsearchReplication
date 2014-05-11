e53486210dd32e622ebb3f277adc47fe06970b93
Itamar Syn-Hershko <itamar@code972.com>
2014-05-11 22:12:17 +0300

    Common pitfall

--- RavenDB.ElasticsearchReplication/Helpers.cs
+++ RavenDB.ElasticsearchReplication/Helpers.cs
@@ -20,6 +20,8 @@ namespace Raven.Bundles.ElasticsearchReplication
 
         public static string GetEmbeddedJson(Assembly assembly, string embeddedResourcePath)
         {
+            if (!embeddedResourcePath.StartsWith(".")) embeddedResourcePath = "." + embeddedResourcePath;
+
             using (var stream = assembly.GetManifestResourceStream(assembly.GetName().Name + embeddedResourcePath))
             using (var reader = new StreamReader(stream))
             {
