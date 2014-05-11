54302fda7c709fed6d440c301b3d346202738a05
Itamar Syn-Hershko <itamar@code972.com>
2014-05-11 19:23:59 +0300

    AssemblyInfo

--- RavenDB.ElasticsearchReplication/Properties/AssemblyInfo.cs
+++ RavenDB.ElasticsearchReplication/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RavenDB.ElasticsearchReplication")]
+[assembly: AssemblyTitle("RavenDB.Bundles.ElasticsearchReplication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RavenDB.ElasticsearchReplication")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Itamar Syn-Hershko")]
+[assembly: AssemblyProduct("RavenDB.Bundles.ElasticsearchReplication")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Itamar Syn-Hershko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
